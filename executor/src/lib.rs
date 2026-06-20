14665d5b5c4162fd6286f6b5fec28f1bcc160964
nuccE541 <10245101541@stu.ecnu.edu.cn>
2026-06-21 02:18:13 +0800

    add test cases of command M for bus

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -3,4 +3,4 @@ mod action;
 mod assembler;
 
 pub use crate::action::pose::Pose;
-pub use crate::executor::{executor::Executor,sports_car_executor::SportsCarExecutor,};
+pub use crate::executor::{executor::Executor,sports_car_executor::SportsCarExecutor,bus_executor::BusExecutor};
